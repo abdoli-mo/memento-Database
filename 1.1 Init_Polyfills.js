598dc4b7ee01d173872ada72de4b602367ac6475
abdoli-mo <m.abdoli10@gmail.com>
2026-05-19 17:33:21 +0330

    Update 1.1 Init_Polyfills.js

--- 1.1 Init_Polyfills.js
+++ 1.1 Init_Polyfills.js
@@ -147,86 +147,117 @@ if (!Array.prototype.min) {
     };
 }
 
-// ========== setTimeout / clearTimeout (جاوا ترد) ==========
-if (typeof setTimeout === 'undefined') {
-    setTimeout = function(callback, delay) {
-        const args = Array.prototype.slice.call(arguments, 2);
-        const thread = new java.lang.Thread({
-            run: function() {
+// =============================================
+// ⏱️ توابع زمانبندی (جاوا) - نیاز به Function Expression به دلیل arguments
+// =============================================
+
+/**
+ * اجرای تابع پس از تاخیر مشخص (جاوا)
+ * @param {Function} callback - تابع برگشتی
+ * @param {number} delay - تاخیر به میلی‌ثانیه
+ * @returns {java.lang.Thread} نخ ایجاد شده
+ */
+function setTimeout(callback, delay) {
+    const args = Array.prototype.slice.call(arguments, 2);
+    const thread = new java.lang.Thread({
+        run: function() {
+            try {
+                java.lang.Thread.sleep(delay);
+                callback.apply(null, args);
+            } catch(e) {
+                // نخ interrupted
+            }
+        }
+    });
+    thread.start();
+    return thread;
+};
+
+/**
+ * لغو اجرای setTimeout
+ * @param {java.lang.Thread} thread - نخ ایجاد شده توسط setTimeout
+ */
+function clearTimeout(thread) {
+    if (thread && thread.isAlive()) {
+        try {
+            thread.interrupt();
+            thread.join(100);
+        } catch(e) {
+            // خطای لغو
+        }
+    }
+};
+
+// ذخیره اینتروال‌های فعال
+const activeIntervals = [];
+
+/**
+ * اجرای تابع به صورت دوره‌ای (جاوا)
+ * @param {Function} callback - تابع برگشتی
+ * @param {number} delay - فاصله زمانی به میلی‌ثانیه
+ * @returns {Object} آبجکت اینتروال با متد stop()
+ */
+function setInterval(callback, delay) {
+    const args = Array.prototype.slice.call(arguments, 2);
+    let running = true;
+    
+    const thread = new java.lang.Thread({
+        run: function() {
+            while (running) {
                 try {
                     java.lang.Thread.sleep(delay);
-                    callback.apply(null, args);
+                    if (running) {
+                        callback.apply(null, args);
+                    }
                 } catch(e) {
-                    // نخ interrupted شد
+                    break;
                 }
             }
-        });
-        thread.start();
-        return thread;
-    };
-}
-
-if (typeof clearTimeout === 'undefined') {
-    clearTimeout = function(thread) {
-        if (thread && thread.isAlive && thread.isAlive()) {
+        }
+    });
+    
+    const intervalObj = {
+        thread: thread,
+        stop: function() {
+            running = false;
             try {
-                thread.interrupt();
-                thread.join(100);
+                if (this.thread && this.thread.isAlive()) {
+                    this.thread.interrupt();
+                    this.thread.join(100);
+                }
             } catch(e) {
-                // خطا در لغو
+                // خطای توقف
+            }
+            const idx = activeIntervals.indexOf(intervalObj);
+            if (idx > -1) {
+                activeIntervals.splice(idx, 1);
             }
         }
     };
-}
+    
+    activeIntervals.push(intervalObj);
+    thread.start();
+    return intervalObj;
+};
 
-// ========== setInterval / clearInterval (جاوا ترد) ==========
-if (typeof setInterval === 'undefined') {
-    let activeIntervals = [];
-    
-    setInterval = function(callback, delay) {
-        const args = Array.prototype.slice.call(arguments, 2);
-        let running = true;
-        
-        const thread = new java.lang.Thread({
-            run: function() {
-                while (running) {
-                    try {
-                        java.lang.Thread.sleep(delay);
-                        if (running) {
-                            callback.apply(null, args);
-                        }
-                    } catch(e) {
-                        break;
-                    }
-                }
-            }
-        });
-        
-        const intervalObj = {
-            thread: thread,
-            stop: function() {
-                running = false;
-                try {
-                    if (this.thread && this.thread.isAlive()) {
-                        this.thread.interrupt();
-                        this.thread.join(100);
-                    }
-                } catch(e) {}
-                const idx = activeIntervals.indexOf(intervalObj);
-                if (idx > -1) {
-                    activeIntervals.splice(idx, 1);
-                }
-            }
-        };
-        
-        activeIntervals.push(intervalObj);
-        thread.start();
-        return intervalObj;
-    };
-    
-    clearInterval = function(interval) {
+/**
+ * لغو اجرای setInterval
+ * @param {Object} interval - آبجکت اینتروال برگشتی از setInterval
+ */
+function clearInterval(interval) {
+    if (interval && interval.stop) {
+        interval.stop();
+    }
+};
+
+/**
+ * پاک کردن تمام اینتروال‌های فعال
+ */
+function cleanupAllIntervals() {
+    while (activeIntervals.length > 0) {
+        const interval = activeIntervals[0];
         if (interval && interval.stop) {
             interval.stop();
         }
-    };
-}
+    }
+};
